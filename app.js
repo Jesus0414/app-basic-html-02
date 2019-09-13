5071147857d4b82b196f91c143042e4e0ee32cb5
Jesus_0414 <jesus140400@gmail.com>
2019-09-13 12:34:11 -0700

    xd

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ const doge = document.getElementById('doge');
 doge.addEventListener('click', ()=>{
     console.log('Guau, guau')
 });
+
+const drumButtons = document.querySelectorAll('button');
+
+drumButtons.forEach(
+        button =>{
+            button.style.left = `${button.dataset.posx}px`; 
+            button.style.top = `${button.dataset.posy}px`;
+        } 
+);
